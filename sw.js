0ad734e93a95797f018c1b274738852d3c904590
calebdeka <calebdeka8@gmail.com>
2026-02-13 12:03:14 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,13 +2,22 @@ const CACHE_NAME = 'cbtech-v1';
 const ASSETS = [
   '/CBtech-l/',
   '/CBtech-l/index.html',
-  '/CBtech-l/manifest.json'
+  '/CBtech-l/manifest.json',
+  '/CBtech-l/sw.js'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((c) => {
+      return c.addAll(ASSETS);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      return r || fetch(e.request);
+    })
+  );
 });
